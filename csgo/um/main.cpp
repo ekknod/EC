20bcb1f8cc1010e365aeca7ed4fc449158be8c24
ekknod <ekknod@gmail.com>
2022-10-19 12:23:43 +0300

    changing the fov back

--- csgo/um/main.cpp
+++ csgo/um/main.cpp
@@ -24,7 +24,7 @@ namespace config
 {
 	BOOL  rcs = 1;
 	DWORD aimbot_button = 110;
-	float aimbot_fov = 180.0f;
+	float aimbot_fov = 2.0f;
 	float aimbot_smooth = 20.0f;
 	BOOL  aimbot_visibility_check = 0;
 	DWORD triggerbot_button = 111;
