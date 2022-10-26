888a66a6d04de63f717e8ab908d2574361cb22a4
ekknod <ekknod@gmail.com>
2022-10-26 20:49:22 +0300

    address validation change

--- apex/shared/apex.cpp
+++ apex/shared/apex.cpp
@@ -268,14 +268,16 @@ static BOOL apex::initialize(void)
 		goto cleanup;
 	}
 
-	IClientEntityList = vm::get_relative_address(apex_handle, IClientEntityList, 3, 7);
-	IClientEntityList = IClientEntityList + 0x08;
-	if (IClientEntityList == (QWORD)0x08)
 	{
-#ifdef DEBUG
-		LOG("[-] failed to find IClientEntityList\n");
-#endif
-		goto cleanup;
+		temp_address = vm::get_relative_address(apex_handle, IClientEntityList, 3, 7);
+		if (temp_address == (IClientEntityList + 0x07))
+		{
+	#ifdef DEBUG
+			LOG("[-] failed to find IClientEntityList\n");
+	#endif
+			goto cleanup;
+		}
+		IClientEntityList = temp_address + 0x08;
 	}
 
 	C_BasePlayer = vm::scan_pattern_direct(apex_handle, apex_base, "\x89\x41\x28\x48\x8B\x05\x00\x00\x00\x00\x48\x85\xC0", "xxxxxx????xxx", 13);
@@ -288,13 +290,18 @@ static BOOL apex::initialize(void)
 	}
 
 	C_BasePlayer = C_BasePlayer + 0x03;
-	C_BasePlayer = vm::get_relative_address(apex_handle, C_BasePlayer, 3, 7);
-	if (C_BasePlayer == 0)
+
+
 	{
-#ifdef DEBUG
-		LOG("[-] failed to find dwLocalPlayer\n");
-#endif
-		goto cleanup;
+		temp_address = vm::get_relative_address(apex_handle, C_BasePlayer, 3, 7);
+		if (temp_address == (C_BasePlayer + 0x07))
+		{
+	#ifdef DEBUG
+			LOG("[-] failed to find dwLocalPlayer\n");
+	#endif
+			goto cleanup;
+		}
+		C_BasePlayer = temp_address;
 	}
 
 	IInputSystem = vm::scan_pattern_direct(apex_handle, apex_base,
