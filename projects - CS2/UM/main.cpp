10e4a9ddf5a68a91a1909fddd1b288772b893fe0
riviello444 <103794561+riviello444@users.noreply.github.com>
2023-10-07 16:02:19 +0200

    Update main.cpp (cfg)

--- projects - CS2/UM/main.cpp
+++ projects - CS2/UM/main.cpp
@@ -18,12 +18,13 @@ namespace input
 	}
 }
 
+
 namespace config
 {
 	BOOL  rcs = 0;
 	DWORD aimbot_button = 314; // mouse left key
-	float aimbot_fov = 2.0f;
-	float aimbot_smooth = 5.0f;
+	float aimbot_fov = 20.0f;
+	float aimbot_smooth = 15.0f;
 	BOOL  aimbot_visibility_check = 0;
 	DWORD triggerbot_button = 318; // mouse forward key
 	BOOL  visuals_enabled = 1;
