b485d3fef2dc77590b4281a8c23e333dd97d75ff
ekknod <ekknod@gmail.com>
2023-10-22 19:12:13 +0300

    using SDL3 in linux

--- projects - CS2/LINUX/main.cpp
+++ projects - CS2/LINUX/main.cpp
@@ -8,44 +8,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#include "../../library/SDL3/include/SDL3/SDL.h"
 
-#include <GL/gl.h>
-#include "../../library/glfw/include/GLFW/glfw3.h"
-
-static const GLFWvidmode *mode;
-
-namespace gl
-{
-	void DrawFillRect(float x, float y, float width, float height, int r, int g, int b)
-	{
-		glBegin(GL_QUADS);
-		glColor4f(r / 255.f, g / 255.f, b / 255.f, 1);
-		glVertex2f(x - (width / 2), y);
-		glVertex2f(x - (width / 2), y - height);
-		glVertex2f(x + (width / 2), y - height);
-		glVertex2f(x + (width / 2), y);
-		glEnd();
-	}
-
-	void DrawRect(float x, float y, float width, float height, int r, int g, int b)
-	{
-		glLineWidth(2);
-		glBegin(GL_LINE_LOOP);
-		glColor4f(r / 255.f, g / 255.f, b / 255.f, 1);
-		glVertex2f(x - (width / 2), y);
-		glVertex2f(x - (width / 2), y - height);
-		glVertex2f(x + (width / 2), y - height);
-		glVertex2f(x + (width / 2), y);
-		glEnd();
-	}
-}
-
-
-
-
-
-
-
+static SDL_Renderer* renderer;
 
 
 static int fd;
@@ -119,40 +84,30 @@ namespace client
 
 	void DrawRect(void *hwnd, int x, int y, int w, int h, unsigned char r, unsigned char g, unsigned char b)
 	{
-		float fl_x = ((float)x / (float)mode->width) * 2.0f;
-		float fl_y = ((mode->height - (float)y) / (float)mode->height) * 2.0f;
+		SDL_FRect rect{};
+		rect.x = (float)x;
+		rect.y = (float)y;
+		rect.w = (float)w;
+		rect.h = (float)h;
+		SDL_SetRenderDrawColor(renderer, r, g, b, 50);
+		SDL_RenderFillRect(renderer, &rect);
 
-		fl_x -= 1.0f;
-		fl_y -= 1.0f;
-
-		float fl_w = ((float)w / mode->width)  * 2.0f;
-		float fl_h = ((float)h / mode->height) * 2.0f;
-
-		fl_x -= (fl_w/2.0f);
-
-		gl::DrawRect(fl_x, fl_y, fl_w, fl_h, r, g, b);
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderRect(renderer, &rect);
 	}
 
 	void DrawFillRect(void *hwnd, int x, int y, int w, int h, unsigned char r, unsigned char g, unsigned char b)
 	{
-		float fl_x = ((float)x / (float)mode->width) * 2.0f;
-		float fl_y = ((mode->height - (float)y) / (float)mode->height) * 2.0f;
-
-		fl_x -= 1.0f;
-		fl_y -= 1.0f;
-
-		float fl_w = ((float)w / mode->width)  * 2.0f;
-		float fl_h = ((float)h / mode->height) * 2.0f;
-
-		fl_x += (fl_w/2.0f);
-
-		gl::DrawFillRect(fl_x, fl_y, fl_w, fl_h, r, g, b);
+		SDL_FRect rect{};
+		rect.x = (float)x;
+		rect.y = (float)y;
+		rect.w = (float)w;
+		rect.h = (float)h;
+		SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+		SDL_RenderFillRect(renderer, &rect);
 	}
 }
 
-//
-// to-do fix: for some reason esp position is little off
-//
 int main(void)
 {
 	fd = open_device("event-mouse", 11);
@@ -162,49 +117,53 @@ int main(void)
 		return 0;
 	}
 
-	if (!glfwInit())
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		return 0;
+	}
+	
+	SDL_Window *temp = SDL_CreateWindow("EC", 1, 1, SDL_WINDOW_MINIMIZED | SDL_WINDOW_OPENGL);
+	if (temp == NULL)
 	{
 		return 0;
 	}
+	SDL_DisplayID id = SDL_GetDisplayForWindow(temp);
+	const SDL_DisplayMode *disp = SDL_GetCurrentDisplayMode(id);
+	SDL_SetWindowPosition(temp, 0, 0);
 
+	SDL_Window *window = SDL_CreatePopupWindow(temp, 0, 0, 640, 480,
+		SDL_WINDOW_TRANSPARENT | SDL_WINDOW_BORDERLESS | SDL_WINDOW_TOOLTIP);
+		
+	SDL_SetWindowSize(window, disp->w, disp->h);
+	SDL_SetWindowPosition(window, 0, 0);
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	SDL_SetWindowAlwaysOnTop(window, SDL_TRUE);
 
-	glfwDefaultWindowHints();
-	glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
-	glfwWindowHint(GLFW_DECORATED, GL_FALSE);
-	glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
-	mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+	renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_PRESENTVSYNC);
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 
-	GLFWwindow *window = glfwCreateWindow(mode->width, mode->height, "EC", NULL, NULL);
-
-	glfwMakeContextCurrent(window);
-
-	glfwSetWindowAttrib(window, GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
-	glfwSetWindowAttrib(window, GLFW_FLOATING, GLFW_TRUE);
-	glfwSetWindowAttrib(window, GLFW_MOUSE_PASSTHROUGH, GLFW_TRUE);
-
-	glfwSwapInterval(1);
-
-	while (!glfwWindowShouldClose(window))
+	BOOL quit = 0;
+	while (!quit)
 	{
-		glfwPollEvents();
+		SDL_Event e;
+		while ( SDL_PollEvent( &e ) )
+		{
+			if (e.type == SDL_EVENT_QUIT)
+			{
+				quit = 1;
+				break;
+			}
+		}
 
-		int w, h;
-		glfwGetFramebufferSize(window, &w, &h);
-		glViewport(0, 0, w, h);
+		SDL_RenderClear(renderer);
 
-		glClearColor(0, 0, 0, 0);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		cs2::run();
 
-		glfwSwapBuffers(window);
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+		SDL_RenderPresent(renderer);
 	}
-	glfwDestroyWindow(window);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
 
