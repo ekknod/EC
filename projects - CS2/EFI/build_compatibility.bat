5e52b48daa26e7d1566180efbfb92da9c39d9b4f
ekknod <ekknod@gmail.com>
2023-11-25 15:36:35 +0200

    added compatibility version

---
+++ projects - CS2/EFI/build_compatibility.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" "bootx64/bootx64_compatibility.sln" /t:Clean,Build /property:Configuration=Release /property:Platform=x64
+copy "bootx64\x64\Release\bootx64.efi" .
